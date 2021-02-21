34ab431ea70c1eea6a647b9235468cb4637c6bf8
John Catlin <johncatlin@Johns-MacBook-Air.local>
2021-02-21 17:11:22 +0000

    .

---
+++ src/templates/set1/FileHeaderTemplate_en_GB.java
@@ -0,0 +1,12 @@
+/*
+ * This file header was injected by the code generator.
+ * 
+ * You can include your own (custom) file header (even an empty one) here by forking the template
+ * set, customising it, and pointing the generator at your custom template.
+ * 
+ * Actually, you can modify any (or all) of the code generation templates using
+ * the same technique.
+ * 
+ * Generation Timestamp: ${.now?datetime}
+ * 
+ */
